ce6413cc592e3d93b661ee2c637219f96fa53cb5
rachhhen <skeletonpanda@mail.ru>
2024-07-31 15:14:43 +0300

    work 5

--- script.sql
+++ script.sql
@@ -1,37 +1,72 @@
-CREATE TABLE gas (
+CREATE TABLE if not exists gas (
 	id			serial PRIMARY KEY,
 	vendor_code	varchar(32) NOT NULL,
 	gas_type	varchar(16) NOT NULL
 );
 
-CREATE TABLE remaining_gas (
-	remain_id integer CONSTRAINT PRIMARY KEY,
-	vendor_code	varchar(32) references gas(vendor_code) NOT NULL,
-	amount	integer
+CREATE TABLE if not exists territory (
+	cadastral_id integer PRIMARY KEY,
+	status varchar(32) NOT NULL,
+	price money NOT NULL,
+	territory_location varchar(100) NOT NULL,
+	average_cars integer NOT NULL
 );
 
-CREATE TABLE client (
-	client_number integer CONSTRAINT PRIMARY KEY,
+CREATE TABLE if not exists station_owner (
+	owner_id integer PRIMARY KEY,
+	owner_name varchar(64) NOT NULL,
+	company_name varchar(64) NOT NULL,
+	ownership_begin_date date NOT NULL,
+	objects_amount integer NOT NULL
+);
+
+CREATE TABLE if not exists gas_station (
+	gas_station_id integer PRIMARY KEY,
+	owner_id integer references station_owner(owner_id) NOT NULL,
+	cadastral_number integer references territory(cadastral_id) NOT NULL,
+	gas_station_type varchar(32) NOT NULL,
+	station_location varchar(100) NOT NULL,
+	pump_amount integer NOT NULL
+);
+
+CREATE TABLE if not exists cashier (
+	cashier_id integer PRIMARY KEY,
+	gas_station_id integer references gas_station(gas_station_id) NOT NULL,
+	cashier_name varchar(64) NOT NULL,
+	qualification varchar(64) NOT NULL,
+	phone_number varchar(20) NOT NULL,
+	work_begin_date date NOT NULL
+);
+
+CREATE TABLE if not exists remaining_gas (
+	remain_id integer PRIMARY KEY,
+	gas_id	integer references gas(id) NOT NULL,
+	amount	decimal
+);
+
+CREATE TABLE if not exists client(
+	client_id integer PRIMARY KEY,
 	client_name	varchar(64) NOT NULL,
-	phone_number	integer NOT NULL,
+	phone_number	varchar(20) NOT NULL,
 	payment_info	varchar(32) NOT NULL,
 	birth_date	date NOT NULL
 );
 
-CREATE TABLE gas_consumption (
-	receipt_number integer CONSTRAINT PRIMARY KEY,
-	vendor_code	varchar(32) references gas(vendor_code) NOT NULL,
-	client_number integer references client(client_number) NOT NULL,
-	total integer NOT NULL,
+CREATE TABLE if not exists gas_consumption (
+	gas_consumption_id integer PRIMARY KEY,
+	cashier_id integer references cashier(cashier_id) NOT NULL,
+	gas_id	integer references gas(id) NOT NULL,
+	client_id integer references client(client_id) NOT NULL,
+	total decimal NOT NULL,
 	total_price money NOT NULL,
-	liters_amount integer NOT NULL,
-	purchase_time time NOT NULL
+	liters_amount decimal NOT NULL,
+	purchase_time timestamp NOT NULL
 );
 
-CREATE TABLE gas_income(
-	income_contract_number integer CONSTRAINT PRIMARY KEY,
-	gas_station_id integer references gas_staion(gas_station_id) NOT NULL,
-	vendor_code varchar references gas(vendor_code) NOT NULL,
+CREATE TABLE if not exists gas_income (
+	income_contract_id integer PRIMARY KEY,
+	gas_station_id integer references gas_station(gas_station_id) NOT NULL,
+	gas_id	integer references gas(id) NOT NULL,
 	rate integer NOT NULL,
 	provider varchar(40) NOT NULL,
 	shipping_company varchar(40) NOT NULL,
@@ -39,36 +74,5 @@ CREATE TABLE gas_income(
 	delivery_date date NOT NULL
 );
 
-CREATE TABLE cashier(
-	cashier_id integer CONSTRAINT PRIMARY KEY,
-	gas_station_id integer references gas_staion(gas_station_id) NOT NULL,
-	cashier_name varchar(64) NOT NULL,
-	qualification varchar(64) NOT NULL,
-	phone_number integer NOT NULL,
-	work_begin_date date NOT NULL
-);
 
-CREATE TABLE gas_station(
-	gas_station_id integer CONSTRAINT PRIMARY KEY
-	inn integer references station_owner(inn) NOT NULL,
-	cadastral_number integer references territory(cadastral_number) NOT NULL,
-	gas_station_type varchar(32) NOT NULL,
-	station_location varchar(100) NOT NULL,
-	pump_amount integer NOT NULL,
-);
 
-CREATE TABLE station_owner(
-	inn integer CONSTRAINT PRIMARY KEY,
-	owner_name varchar(64) NOT NULL,
-	company_name varchar(64) NOT NULL,
-	ownership_begin_date date NOT NULL,
-	objects_amount integer NOT NULL
-);
-
-CREATE TABLE territory(
-	cadastral_number integer CONSTRAINT PRIMARY KEY,
-	status varchar(32) NOT NULL,
-	price money NOT NULL,
-	territory_location varchar(100) NOT NULL,
-	average_cars integer NOT NULL
-);
